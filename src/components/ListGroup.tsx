b5ae8962e2e0f4a62832ee248a7cc88f9521a26c
karim <karim.tarek96@yahoo.com>
2024-12-08 14:30:30 +0200

    clickable list items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,19 @@
 function ListGroup() {
   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  items = [];
-  if (items.length == 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No items found</p>
-      </>
-    );
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("clicked")}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
